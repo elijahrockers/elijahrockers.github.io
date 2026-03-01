da0e1478364d6a3efa3eda941bb25d79e0a68846
erockers3 <erockers3@gatech.edu>
2026-03-01 21:11:14 +0000

    deployment tinkering...

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://elijahrockers.github.io',
-  base: '/elijahrockers/',
+  //base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
